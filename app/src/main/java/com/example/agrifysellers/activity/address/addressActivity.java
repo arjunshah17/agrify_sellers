5f3d4b5ac4b1719d3a9fd1ba0e88c5379e83b3b3
arjun shah <arjun.18shah@gmail.com>
2019-03-27 23:27:05 +0530

    add todo

---
+++ app/src/main/java/com/example/agrifysellers/activity/address/addressActivity.java
@@ -0,0 +1,16 @@
+package com.example.agrifysellers.activity.address;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agrifysellers.R;
+
+public class addressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+    }
+}
---
+++ app/src/main/java/com/example/agrifysellers/activity/address/model/Address.java
@@ -0,0 +1,5 @@
+package com.example.agrifysellers.activity.address.model;
+
+public class Address {
+    //TODO make model class for address
+}
